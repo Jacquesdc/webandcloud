f516e03d65be3c08df0e68084f63b63ab304fe26
jacquesdc <jacques.de.chastenet@gmail.com>
2020-11-14 21:05:24 +0000

    server starting

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,54 +1,25 @@
 
-// Code goes here
 
+const store = {
+    user : {},
+    id_token: "",
+};
 
-// Code goes here
+const update = () => {
+    $("#store-value").text(JSON.stringify(store));
+}
+const signOut = () => {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        store.user = {};
+        update();
+    });
+}
+const onSignIn = (googleUser) => {
+    store.user = googleUser.getBasicProfile();
+    store.id_token = googleUser.getAuthResponse().id_token;
+    update();
+}
 
-var MyApp = (function (superclass) {
-  function MyApp(props) {
-    superclass.call(this, props);
-
-    this.state = {
-      data: [],
-    };
-  }
-
-  if ( superclass ) MyApp.__proto__ = superclass;
-  MyApp.prototype = Object.create( superclass && superclass.prototype );
-  MyApp.prototype.constructor = MyApp;
-
-  MyApp.prototype.componentDidMount = function componentDidMount () {
-    var this$1 = this;
-
-    fetch('_ah/api/myApi/v1/scores')
-      .then(function (response) { return response.json(); })
-      .then(function (data) { return this$1.setState({  data : data.items }); });
-  };
-  MyApp.prototype.render = function render () {
-    return React.createElement( 'ul', null, this.state.data.map(function (e) { return React.createElement( 'li', null, " ", e.properties.name, " ", e.properties.score, " " ); }), "  " );
-  };
-
-  
-//written in JSX...
-//  XomponentDidMount() {
-//	    fetch('https://sobike44.appspot.com/_ah/api/myApi/v1/scores')
-//	      .then(response => response.json())
-//	      .then(data => this.setState({  data : data.items }));
-//	  }
-//	  render() {
-//	    return <ul>{this.state.data.map(e => <li> {e.properties.name} {e.properties.score} </li>)}  </ul>;
-//	  }
-//	}
-  
-  
-  return MyApp;
-}(React.Component));
-
-ReactDOM.render(
-React.createElement('div', null,
-    React.createElement(MyApp)
-  )
-,
-  document.getElementById('app')
-);
-
+update();
